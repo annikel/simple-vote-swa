c903bb41e1f7406f425277334168cef08e59d8ac
annikel <92825476+annikel@users.noreply.github.com>
2022-06-04 12:10:11 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
 
+function sendVote() {
+  fetch('/api/vote')
+    .then(response => response.json())
+    .then(json => alert(json.text));
+}
+
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  return (
+    <div className="App">
+      <button onClick={sendVote}>Vote</button>
+    </div>
+  );
 }
 
 export default App;
